c5a92053f33573157de2a26dece171f432609e35
Wong Lok <yellowhappy831@gmail.com>
2019-01-30 04:14:32 +0800

    github backup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="full" id="app">
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
@@ -17,12 +16,17 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0px;
+}
+.full{
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,11 @@
 import Vue from 'vue'
 
+const Components = {
+  EnosBanner: require('./enos/Banner.vue').default
+}
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name])
+})
+
+export default Components
